77c033565b93892ea767ab5d91a82d57db8c0a53
VikramShawFL <vikram@front.live>
2019-09-16 17:10:38 +0530

    Server Error Connection

--- src/xtnt_auth/core.clj
+++ src/xtnt_auth/core.clj
@@ -1,14 +1,19 @@
 (ns xtnt-auth.core
   (:require [compojure.core                 :refer [defroutes ANY POST]]
-            [ring.adapter.jetty     :as jetty]
+            [compojure.route                :refer [not-found resources]]
+            [ring.adapter.jetty             :as    jetty]
+            [ring.middleware.reload         :refer [wrap-reload]]
             [ring.middleware.params         :refer [wrap-params]]
+            [ring.middleware.resource       :refer [wrap-resource]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.middleware.json           :refer [wrap-json-response wrap-json-params]]
             [xtnt-auth.datasource           :refer [get-ds]]
             [xtnt-auth.handlers             :as    handlers]))
 
 (defroutes app-routes
-  (POST "/create-auth-token" [] handlers/create-auth-token))
+  (POST "/create-auth-token" [] handlers/create-auth-token)
+  (resources "/")
+  (not-found "Not Found"))
 
 
 (defn wrap-datasource [handler]
@@ -25,10 +30,12 @@
       wrap-datasource
       wrap-config
       wrap-keyword-params
+      (wrap-resource "public")
       wrap-json-params
       wrap-json-response))
 
 (defn -main []
-	(jetty/run-jetty app {:port 8000})
+	(jetty/run-jetty (wrap-reload #'app)
+                   {:port 8000})
   (println (str "Server is running on port 8000")))
 
